c0be81c6cdc947c5f9c7975be9f54fa98a53e766
scott.ellington <scott.ellington@1f0a84dc-7f99-11dd-9299-37fd5bf03096>
2008-09-30 23:03:47 +0000

    add proj4 wrrapper

--- cumberland/Cumberland.DrawMap/Main.cs
+++ cumberland/Cumberland.DrawMap/Main.cs
@@ -34,20 +34,46 @@ namespace Cumberland.DrawMap
 		public static void Main(string[] args)
 		{
 			MapRenderer map = new MapRenderer();
-			map.Extents = new Rectangle(-115, 14, -87, 34);
-
-			map.Width = 800;
-			map.Height = 800;
+			//map.Extents = new Rectangle(-115, 14, -87, 34);
+			map.Extents = new Rectangle(-85, 29, -81, 31);
+			
+			map.Width = 400;
+			map.Height = 400;
 			
 			map.Layers.Add(new Shapefile("/home/scottell/gis/data/world_adm0/world_adm0.shp"));
 			map.Layers.Add(new Shapefile("/home/scottell/Projects/cumberland/Cumberland.Tests/shape_eg_data/mexico/states.shp"));
 			map.Layers.Add(new Shapefile("/home/scottell/Projects/cumberland/Cumberland.Tests/shape_eg_data/mexico/roads.shp"));
 			map.Layers.Add(new Shapefile("/home/scottell/Projects/cumberland/Cumberland.Tests/shape_eg_data/mexico/cities.shp"));
 			
+			Shapefile shp = new Shapefile("/home/scottell/gis/data/florida/cntshr/cntshr.shp");
+
+			using (ProjFourWrapper proj = new ProjFourWrapper("+proj=aea +lat_1=24 +lat_2=31.5 +lat_0=24 +lon_0=-84 +x_0=400000 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m"))
+			{
+				
+				foreach (Feature f in shp.Features)
+				{
+					Polygon p = f as Polygon;
+					foreach (Ring r in p.Rings)
+					{
+						for (int ii=0; ii<r.Points.Count; ii++)
+						{
+							r.Points[ii] = proj.ConvertToLatLong(r.Points[ii]);
+//							System.Console.WriteLine(r.Points[ii]);
+						}
+					}
+				}
+				
+			}
+			map.Layers.Add(shp);
+			
 			Bitmap b = map.Draw();
 			
 			b.Save("/home/scottell/Desktop/test.png", ImageFormat.Png);
-			//b.Save("/home/scottell/Desktop/test.bmp", ImageFormat.Bmp);
+		
+	
+		
+			       
+			       
 		}
 	}
 }
