0a6687286c75d6e4853f81246b06def3f0e0b3ab
scott.ellington <scott.ellington@1f0a84dc-7f99-11dd-9299-37fd5bf03096>
2008-11-03 02:40:38 +0000

    * cumberland/WellKnownText.cs, Cumberland.Tests/WellKnownTextTests.cs:   add create WKT methods * cumberland/Data/Shapefile/DBaseIIIFile.cs: remove warnings

--- cumberland/Cumberland.Tests/WellKnownTextTests.cs
+++ cumberland/Cumberland.Tests/WellKnownTextTests.cs
@@ -34,31 +34,86 @@ namespace Cumberland.Tests
 	public class WellKnownTextTests
 	{
 		[Test]
-		public void TestWKTPoint()
+		public void TestWKTToPoint()
 		{
 			string wkt = "POINT(0 0)";
 			
 			Point p = WellKnownText.ParsePoint(wkt);
-			
 			Assert.AreEqual(new Point(0,0), p);
 		}
 		
 		[Test]
-		public void TestWKTMultLineString()
+		public void TestWKTToPolyLine()
 		{
 			string wkt = "MULTILINESTRING((0 0 0,1 1 0,1 2 1),(2 3 1,3 2 1,5 4 1))";
 			
-			WellKnownText.ParseMultiLineString(wkt);
+			PolyLine l = WellKnownText.ParseMultiLineString(wkt);
+			Assert.AreEqual(2, l.Lines.Count);
 		}
 		
 		[Test]
-		public void TestWKTMultiPolygon()
+		public void TestWKTPolygons()
 		{
 			string wkt = "MULTIPOLYGON(((0 0 0,4 0 0,4 4 0,0 4 0,0 0 0),(1 1 0,2 1 0,2 2 0,1 2 0,1 1 0)),((-1 -1 0,-1 -2 0,-2 -2 0,-2 -1 0,-1 -1 0)))";
 			
 			List<Feature> l = WellKnownText.ParseMultiPolygon(wkt);
+			Assert.AreEqual(2, l.Count);
+		}
+		
+		[Test]
+		public void TestPolygonToWKT()
+		{
+			Polygon p = new Polygon();
+			Ring r1 = new Ring();
+			r1.Points.Add(new Point(0,0));
+			r1.Points.Add(new Point(5,0));
+			r1.Points.Add(new Point(5,6));
+			r1.Points.Add(new Point(0,5));
+			r1.Close();
+			p.Rings.Add(r1);
 			
-			Assert.IsTrue(l.Count == 2);
+			Assert.AreEqual("POLYGON((0 0,5 0,5 6,0 5,0 0))", WellKnownText.CreateFromPolygon(p));
+			
+			Ring r2 = new Ring();
+			r2.Points.Add(new Point(1.1,1.1));
+			r2.Points.Add(new Point(1.6,1.1));
+			r2.Points.Add(new Point(1.4,1.4));
+			r2.Points.Add(new Point(1.6,1.1));
+			r2.Close();
+			p.Rings.Add(r2);
+			
+			Assert.AreEqual("POLYGON((0 0,5 0,5 6,0 5,0 0)(1.1 1.1,1.6 1.1,1.4 1.4,1.6 1.1,1.1 1.1))", 
+			                WellKnownText.CreateFromPolygon(p));
+		}
+		
+		[Test]
+		public void TestPolyLineToWKT()
+		{
+			PolyLine p = new PolyLine();
+			Line l1 = new Line();
+			l1.Points.Add(new Point(0,0));
+			l1.Points.Add(new Point(5,0));
+			l1.Points.Add(new Point(5,6));
+			l1.Points.Add(new Point(0,5));
+			p.Lines.Add(l1);
+			
+			Assert.AreEqual("MULTILINESTRING((0 0,5 0,5 6,0 5))", WellKnownText.CreateFromPolyLine(p));
+			
+			Line l2 = new Line();
+			l2.Points.Add(new Point(1.1,1.1));
+			l2.Points.Add(new Point(1.6,1.1));
+			l2.Points.Add(new Point(1.4,1.4));
+			l2.Points.Add(new Point(1.6,1.1));
+			p.Lines.Add(l2);
+			
+			Assert.AreEqual("MULTILINESTRING((0 0,5 0,5 6,0 5)(1.1 1.1,1.6 1.1,1.4 1.4,1.6 1.1))", 
+			                WellKnownText.CreateFromPolyLine(p));
+		}
+		
+		public void TestPointToWKT()
+		{
+			Point p = new Point(3,4);
+			Assert.AreEqual("POINT(3 4)", WellKnownText.CreateFromPoint(p));
 		}
 	}
 }
